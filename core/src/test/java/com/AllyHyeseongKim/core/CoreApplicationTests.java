c16857f8e6aac40aebb6ba7cff536bc10321cc20
AllyHyeseongKim <ally3k@icloud.com>
2022-06-07 17:42:52 +0900

    [develop] Create Spring Initializer
    core

---
+++ core/src/test/java/com/AllyHyeseongKim/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AllyHyeseongKim.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
