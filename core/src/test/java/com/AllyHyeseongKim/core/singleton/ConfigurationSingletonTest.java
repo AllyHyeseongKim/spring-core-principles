56c74823f801bdc8359d4a8857b4e2f653e4bfde
AllyHyeseongKim <ally3k@icloud.com>
2022-06-11 21:38:08 +0900

    [feature/singleton] Update the singleton container
    core/src/main/test/java/com/AllyHyeseongKim/core/singleton

---
+++ core/src/test/java/com/AllyHyeseongKim/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,42 @@
+package com.AllyHyeseongKim.core.singleton;
+
+import com.AllyHyeseongKim.core.AppConfig;
+import com.AllyHyeseongKim.core.member.MemberRepository;
+import com.AllyHyeseongKim.core.member.MemberService;
+import com.AllyHyeseongKim.core.member.MemberServiceImpl;
+import com.AllyHyeseongKim.core.order.OrderService;
+import com.AllyHyeseongKim.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
+}
