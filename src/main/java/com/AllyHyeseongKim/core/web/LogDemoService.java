d689fd490c0ffd4dca18f1db833ec1fe63780363
AllyHyeseongKim <ally3k@icloud.com>
2022-06-13 17:18:07 +0900

    [feature/scope] Update the bean scope
    src/main/java/com/AllyHyeseongKim/core/common
    src/main/java/com/AllyHyeseongKim/core/web
    src/test/java/com/AllyHyeseongKim/core/scope
    src/main/java/com/AllyHyeseongKim/core/common
    src/build.gradle

---
+++ src/main/java/com/AllyHyeseongKim/core/web/LogDemoService.java
@@ -0,0 +1,17 @@
+package com.AllyHyeseongKim.core.web;
+
+import com.AllyHyeseongKim.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
